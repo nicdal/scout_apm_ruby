c53fb5107b80c666868613037fbe007f8e7c86cc
David Anderson <dave@dubkat.com>
2015-10-20 14:54:54 -0600

    1.9.3 has awful stack trace handling

--- lib/scout_apm/instruments/net_http.rb
+++ lib/scout_apm/instruments/net_http.rb
@@ -25,6 +25,8 @@ module ScoutApm
               self.class.instrument("HTTP/request", :desc => "#{(@address+args.first.path.split('?').first)[0..99]}") do
                 request_without_scout_instruments(*args,&block)
               end
+              rescue SystemStackError => e
+                ScoutApm::Agent.instance.logger.error "Exception in Net::HTTP (#{self.class.to_s}): #{e.message}\nBacktrace: #{e.backtrace}\n\nCaller: #{caller.join("\n")}"
             end
             alias request_without_scout_instruments request
             alias request request_with_scout_instruments
